af5dc85ffdd9ddb48d7abe9ff7c583a6c98e1272
kotla Govardhanarao <168724470+Govardhanaraokotla@users.noreply.github.com>
2024-09-10 13:33:00 +0530

    Create Find a string.py

---
+++ Find a string.py
@@ -0,0 +1,10 @@
+def count_substring(string, sub_string):
+    count=0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count=count+1
+    return count
+    
+    
+   
+
