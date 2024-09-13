e37556f5120842c2c565d3c356402a1d98345502
kotla Govardhanarao <168724470+Govardhanaraokotla@users.noreply.github.com>
2024-09-13 11:30:17 +0530

    Create String Formatting.py

---
+++ String Formatting.py
@@ -0,0 +1,10 @@
+def print_formatted(number):
+    # your code goes here
+    width = len(bin(number)[2:])
+    for i in range(1, number+1):
+        deci = str(i)
+        octa = oct(i)[2:]
+        hexa = hex(i)[2:].upper()
+        bina = bin(i)[2:]
+        print(deci.rjust(width),octa.rjust(width),hexa.rjust(width),bina.rjust(width))
+
