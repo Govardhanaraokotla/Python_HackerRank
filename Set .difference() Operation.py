794f94b948369e7d7bc59b86ac6a1003adc3dadd
kotla Govardhanarao <168724470+Govardhanaraokotla@users.noreply.github.com>
2024-10-23 11:18:07 +0530

    Create Set .difference() Operation.py

---
+++ Set .difference() Operation.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n=int(input())
+n_lst=set(map(int,input().split()))
+n1=int(input())
+n1_lst=set(map(int,input().split()))
+total_lst=n_lst.difference(n1_lst)
+print(len(total_lst))
+
+
