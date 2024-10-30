a2e227638cf0976eae635ea4698c9f29a0d3a78f
kotla Govardhanarao <168724470+Govardhanaraokotla@users.noreply.github.com>
2024-10-30 11:09:31 +0530

    Create Check Strict Superset.py

---
+++ Check Strict Superset.py
@@ -0,0 +1,13 @@
+def isstrictsuperset(a,b):
+    # true if a is a strict superset of b
+    return b.issubset(a) and not(a.issubset(b))
+
+a = set(int(x) for x in input().split(' '))
+n = int(input())
+res = True
+
+for _ in range(n):
+    b = set(int(x) for x in input().split(' '))
+    res &= isstrictsuperset(a,b)
+    
+print(res)
