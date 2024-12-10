bdeb501026091ad8e4cbec85a92187d3021062b3
kotla Govardhanarao <168724470+Govardhanaraokotla@users.noreply.github.com>
2024-12-10 21:20:18 +0530

    Create XML2 - Find the Maximum Depth.py

---
+++ XML2 - Find the Maximum Depth.py
@@ -0,0 +1,10 @@
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    if (level == maxdepth):
+        maxdepth += 1
+        
+    for i in elem:
+        depth(i, level + 1)
+
